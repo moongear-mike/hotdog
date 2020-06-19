5e2f3bede28ba0f66168d117ecc030ffe2868ace
moongear-mike <16594103+moongear-mike@users.noreply.github.com>
2020-06-19 11:34:56 -0400

    Minor Updates

--- includes/footer.php
+++ includes/footer.php
@@ -7,9 +7,7 @@
         ® 2016 Dirty Dogs all rights reserved
     </div>
     <div class="address">
-        274 Marconi Blvd. Columbus, Ohio 43215 <span class="seperator"></span> <a href="tel:+1-614-538-0095"><span
-                    class="read-me-only">Call Dirty Dogs at </span> 614.538.0095</a> <span class="seperator"></span> <a
-                href="/contact/">Contact Us<span class="read-me-only"> at Dirty Dogs</span></a>
+        Somthing useful
     </div>
     <div class="stop-float"></div>
 </div>
